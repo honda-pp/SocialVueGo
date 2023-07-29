08aa0bdb71bacf0c8f7c31b445c40b7e2778290b
honda-pp <s.honda@keio.jp>
2023-07-29 19:39:17 +0900

    [refactor] split main.go

---
+++ backend/app/application.go
@@ -0,0 +1,57 @@
+package app
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/honda-pp/SocialVueGo/backend/app/handlers"
+	"github.com/honda-pp/SocialVueGo/backend/app/repositories"
+	"github.com/honda-pp/SocialVueGo/backend/app/usecases"
+	"github.com/honda-pp/SocialVueGo/backend/infrastructure/database"
+	"github.com/honda-pp/SocialVueGo/backend/infrastructure/logger"
+)
+
+type Application struct {
+	Router *gin.Engine
+}
+
+func NewApplication() *Application {
+	log := logger.InitLogger()
+	db, err := database.InitDB()
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	defer db.Close()
+
+	userRepo := repositories.NewUserRepository(db)
+	userUsecase := usecases.NewUserUsecase(*userRepo)
+	userHandler := handlers.NewUserHandler(*userUsecase)
+
+	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:88"}
+	config.AllowCredentials = true
+	router.Use(cors.New(config))
+
+	store := cookie.NewStore([]byte("secret"))
+	store.Options(sessions.Options{Domain: "localhost", Path: "/api/", MaxAge: 86400})
+	router.Use(sessions.Sessions("mysession", store))
+
+	api := router.Group("/api")
+	{
+		api.POST("/login", userHandler.Login)
+		api.POST("/logout", userHandler.Logout)
+		api.POST("/signup", userHandler.Signup)
+		api.GET("/checkLoggedIn", userHandler.CheckLoggedIn)
+	}
+
+	return &Application{
+		Router: router,
+	}
+}
+
+func (app *Application) Run(addr string) error {
+	return app.Router.Run(addr)
+}
--- backend/main.go
+++ backend/main.go
@@ -1,52 +1,13 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
-	"github.com/gin-gonic/gin"
-	"github.com/honda-pp/SocialVueGo/backend/app/handlers"
-	"github.com/honda-pp/SocialVueGo/backend/app/repositories"
-	"github.com/honda-pp/SocialVueGo/backend/app/usecases"
-	"github.com/honda-pp/SocialVueGo/backend/infrastructure/database"
-	"github.com/honda-pp/SocialVueGo/backend/infrastructure/logger"
+	"github.com/honda-pp/SocialVueGo/backend/app"
 )
 
 func main() {
-	log := logger.InitLogger()
-	db, err := database.InitDB()
+	app := app.NewApplication()
+	err := app.Run(":8080")
 	if err != nil {
-		log.Fatalf("Failed to initialize database: %v", err)
-	}
-	defer db.Close()
-
-	userRepo := repositories.NewUserRepository(db)
-
-	userUsecase := usecases.NewUserUsecase(*userRepo)
-
-	router := gin.Default()
-
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:88"}
-	config.AllowCredentials = true
-	router.Use(cors.New(config))
-
-	store := cookie.NewStore([]byte("secret"))
-	store.Options(sessions.Options{Domain: "localhost", Path: "/api/", MaxAge: 86400})
-	router.Use(sessions.Sessions("mysession", store))
-
-	userHandler := handlers.NewUserHandler(*userUsecase)
-
-	api := router.Group("/api")
-	{
-		api.POST("/login", userHandler.Login)
-		api.POST("/logout", userHandler.Logout)
-		api.POST("/signup", userHandler.Signup)
-		api.GET("/checkLoggedIn", userHandler.CheckLoggedIn)
-	}
-
-	err = router.Run(":8080")
-	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		panic(err)
 	}
 }

5a1a0ca115a655e8d8b5fb4ef8587b2a979a2474
honda-pp <s.honda@keio.jp>
2023-11-19 10:38:05 +0900

    [refactor] tweet list to component

--- frontend/src/views/TweetList.vue
+++ frontend/src/views/TweetList.vue
@@ -2,17 +2,7 @@
   <h1>Tweet List</h1>
   <button @click="showTweetPopup">Create Tweet</button>
 
-  <ul class="tweet-list">
-    <li v-for="tweet in tweetList" :key="tweet.tweet_id" class="tweet-item">
-      <div class="tweet-content">{{ tweet.content }}</div>
-      <div class="tweet-info">
-        <router-link :to="`/${tweet.userID}`" class="user-link">
-          <span class="tweet-username">{{ tweet.username }}</span>
-        </router-link>
-        <span class="tweet-date">{{ formatDate(tweet.createdAt) }}</span>
-      </div>
-    </li>
-  </ul>
+  <TweetList :tweetList="tweetList" />
 
   <teleport to="body">
     <div v-if="isTweetPopupVisible" class="popup-overlay">
@@ -35,12 +25,13 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { getTweetList, createTweet } from '../api/tweetApi';
-import { formatDate } from '../utils/dateUtil';
+import TweetList from '../components/TweetList.vue';
 
 const tweetList = ref([]);
 const isTweetPopupVisible = ref(false);
 const tweetContent = ref('');
 const errorMessage = ref('');
+
 
 onMounted(async () => {
   try {
@@ -95,46 +86,6 @@ const enableScroll = () => {
 </script>
 
 <style>
-.tweet-list {
-  margin-top: 20px;
-  padding: 0;
-  list-style: none;
-}
-
-.tweet-item {
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 15px;
-  margin-bottom: 15px;
-  background-color: #fff;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-.tweet-content {
-  font-size: 1.2rem;
-  margin-bottom: 10px;
-}
-
-.tweet-info {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.tweet-username {
-  font-weight: bold;
-}
-
-.tweet-date {
-  color: #888;
-}
-
-.user-link {
-  text-decoration: none;
-  color: #3498db;
-  margin-right: 20px;
-}
-
 span {
   margin-right: 10px;
 }
